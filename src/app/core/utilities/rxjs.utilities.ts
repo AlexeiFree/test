5921eaa2829ce14c449f8a23762abd0a1c0f4f6b
Alexei <alexeipochta@mail.ru>
2020-06-05 09:49:48 +0500

    asyncArraySwitchMap code reduced

--- src/app/core/utilities/rxjs.utilities.ts
+++ src/app/core/utilities/rxjs.utilities.ts
@@ -1,12 +1,11 @@
 import { of } from 'rxjs';
-import { delay, expand, map, switchMap, takeWhile } from 'rxjs/operators';
+import { delay, expand, switchMap, takeWhile } from 'rxjs/operators';
 
 
 export const asyncArraySwitchMap = <T>(chunkSize = 10) =>
   switchMap((initialArray: T[]) =>
-    of(initialArray)
+    of(initialArray.slice(0, chunkSize))
       .pipe(
-        map(array => array.slice(0, chunkSize)),
         expand(arrayChunk => {
           if (arrayChunk.length < initialArray.length) {
             return of(initialArray.slice(0, arrayChunk.length + chunkSize)).pipe(delay(0));
