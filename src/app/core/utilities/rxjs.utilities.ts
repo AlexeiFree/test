52665c1624b33e9d9675d594bbaeb42b116ff813
Alexei <alexeipochta@mail.ru>
2020-06-05 09:40:15 +0500

    asyncArraySwitchMap code reduced

--- src/app/core/utilities/rxjs.utilities.ts
+++ src/app/core/utilities/rxjs.utilities.ts
@@ -1,29 +1,17 @@
 import { of } from 'rxjs';
 import { delay, expand, map, switchMap, takeWhile } from 'rxjs/operators';
 
-interface IAsyncArrayExpandData<T> {
-  array: T[];
-  arrayChunk: T[];
-}
 
 export const asyncArraySwitchMap = <T>(chunkSize = 10) =>
   switchMap((initialArray: T[]) =>
     of(initialArray)
       .pipe(
-        map(array => ({
-          array,
-          arrayChunk: array.slice(0, chunkSize)
-        }) as IAsyncArrayExpandData<T>),
-        expand(data => {
-          if (data.arrayChunk.length < data.array.length) {
-            return of({
-              array: data.array,
-              arrayChunk: data.array.slice(0, data.arrayChunk.length + chunkSize)
-            } as IAsyncArrayExpandData<T>)
-              .pipe(delay(0));
+        map(array => array.slice(0, chunkSize)),
+        expand(arrayChunk => {
+          if (arrayChunk.length < initialArray.length) {
+            return of(initialArray.slice(0, arrayChunk.length + chunkSize)).pipe(delay(0));
           }
         }),
-        takeWhile(data => data.arrayChunk.length < data.array.length, true),
-        map(data => data.arrayChunk)
+        takeWhile(arrayChunk => arrayChunk.length < initialArray.length, true),
       )
   );
